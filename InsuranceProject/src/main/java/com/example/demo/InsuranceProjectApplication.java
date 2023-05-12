27d9261df65ae659723ad00abfcd904026d11e92
suraj <surajgoregaonkar97@gmail.com>
2023-05-12 15:45:59 +0530

    task done by suraj

--- InsuranceProject/src/main/java/com/example/demo/InsuranceProjectApplication.java
+++ InsuranceProject/src/main/java/com/example/demo/InsuranceProjectApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InsuranceProjectApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(InsuranceProjectApplication.class, args);
+		
+		System.out.println("hello");
 	}
 
+	
 }
