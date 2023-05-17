fc813089e89c739cede10b509b4c8bb9dde6b369
shital <mundeshital@gmail.com>
2023-05-17 22:16:42 +0530

    Get premium details done by shital

---
+++ InsuranceProject/src/main/java/com/insurance/controller/PremiumController.java
@@ -0,0 +1,27 @@
+package com.insurance.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.insurance.model.Premium;
+import com.insurance.service.PremiumService;
+//Design API to get all the premium details from database
+@RestController
+@RequestMapping
+public class PremiumController {
+	@Autowired
+	private PremiumService premiumService;
+
+	@GetMapping("/get/{id}")
+	public ResponseEntity<Premium> getPremiumById(@PathVariable("id") Integer id) {
+
+		Premium premium = premiumService.getPremiumById(id);
+		return ResponseEntity.ok().body(premium);
+
+	}
+
+}
---
+++ InsuranceProject/src/main/java/com/insurance/repository/PremiumRepository.java
@@ -0,0 +1,15 @@
+package com.insurance.repository;
+
+import java.io.Serializable;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.insurance.model.Premium;
+
+@Repository
+public interface PremiumRepository extends CrudRepository<Premium,Serializable> {
+
+	public Premium findById(Integer Id);
+	
+}
